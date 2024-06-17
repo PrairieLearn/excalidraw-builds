fd37afbdc970c442dc8702213805a3a610019c7d
Karthik Nishanth <nishanthkarthik@live.com>
2024-06-17 11:24:04 -0700

    Add implementation from PrairieLearn/PrairieLearn (#1)
    * Add implementation from PrairieLearn/PrairieLearn
    
    Derives from https://github.com/PrairieLearn/PrairieLearn/pull/9900
    
    * remove unnecessary eslint ignore
    
    * Remove unused EXCALIDRAW_ASSET_PATH
    
    We are bundling all necessary assets into JS/CSS so that we no
    longer need to statically serve an `excalidraw-dist/`. In case
    of a package upgrade in the future breaking this contract, we
    could either point the URL to a non-existent path to catch errors
    or let it fall back to the default unpkg mirror. We choose the
    latter.

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+
+import * as ExcalidrawLib from '@excalidraw/excalidraw';
+
+export { React, ReactDOM, ExcalidrawLib };
